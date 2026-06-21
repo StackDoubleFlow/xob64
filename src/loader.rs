2ff77c7a7738c360e69a01a24c6f2b7d70af057c
StackDoubleFlow <ojaslandge@gmail.com>
2026-06-20 22:25:39 -0500

    Fix object segment loading

--- src/loader.rs
+++ src/loader.rs
@@ -12,7 +12,8 @@ use object::{
     read::elf::{ElfFile64, ElfSegment64},
 };
 
-const PAGE_SIZE: usize = nix::libc::_SC_PAGE_SIZE as usize;
+const PAGE_SIZE: LazyLock<usize> =
+    LazyLock::new(|| unsafe { nix::libc::sysconf(nix::libc::_SC_PAGE_SIZE) as usize });
 
 #[derive(Default)]
 struct ObjectPool {
@@ -52,13 +53,13 @@ impl EmuObject {
 static OBJECT_POOL: LazyLock<Arc<Mutex<ObjectPool>>> =
     LazyLock::new(|| Arc::new(Mutex::new(ObjectPool::default())));
 
-unsafe fn load_segment(segment: ElfSegment64, fd: i32, base_addr: &mut *const u8) {
-    if base_addr.is_null() && segment.address() != 0 {
-        panic!("first segment is not at 0");
-    }
+fn align_to_next_page(addr: usize) -> usize {
+    (addr + *PAGE_SIZE - 1) & !(*PAGE_SIZE - 1)
+}
 
+unsafe fn load_segment(segment: ElfSegment64, fd: i32, base_addr: *const u8) {
     let addr = unsafe { base_addr.add(segment.address() as usize) };
-    let page_offset = *base_addr as usize % PAGE_SIZE;
+    let page_offset = addr as usize % *PAGE_SIZE;
     let aligned_addr = unsafe { addr.offset(-(page_offset as isize)) };
     let aligned_size = page_offset + segment.size() as usize;
 
@@ -72,12 +73,16 @@ unsafe fn load_segment(segment: ElfSegment64, fd: i32, base_addr: &mut *const u8
 
     let file_offset = segment.file_range().0;
     let file_size = segment.file_range().1;
-
-    println!("segment addr: {:x}", segment.address());
-    println!("segment align: {:x}", segment.align());
+    // The map offset determines where in the file the mapping starts.
+    // Since the mapping starts at some `page_offset` before the segment address,
+    // we need to adjust the file offset accordingly.
+    let map_offset = file_offset as i64 - page_offset as i64;
+    if map_offset < 0 {
+        panic!("file mapping starts before page mapping: {}", map_offset);
+    }
 
     println!(
-        "Attempting mapping at address {:?} with size {}",
+        "Attempting mapping at address {:?} with size {:#x}",
         aligned_addr, aligned_size
     );
     // Map segment
@@ -86,28 +91,62 @@ unsafe fn load_segment(segment: ElfSegment64, fd: i32, base_addr: &mut *const u8
             aligned_addr as *mut nix::libc::c_void,
             aligned_size,
             prot,
-            nix::libc::MAP_PRIVATE,
+            nix::libc::MAP_PRIVATE | nix::libc::MAP_FIXED_NOREPLACE,
             fd,
-            file_offset as i64,
+            map_offset,
         )
-    };
-    dbg!(mapped_addr);
+    } as *const u8;
+    if mapped_addr as isize == -1 {
+        unsafe {
+            let c_str = std::ffi::CString::new("mmap failed").unwrap();
+            nix::libc::perror(c_str.as_ptr());
+        }
+        panic!("mapping failed");
+    }
+
+    println!("mapping successful: {:?}", mapped_addr);
 
     // If the file mapping ends before the segment does, then we need to zero out the rest
     if segment.size() > file_size {
         unsafe {
-            let segment_end = mapped_addr.add(aligned_size);
-            let mapping_end =
-                ((segment_end as usize + PAGE_SIZE - 1) & !(PAGE_SIZE - 1)) as *const u8;
-            let file_load_end = mapped_addr.add(page_offset + file_size as usize);
-            let zero_len = mapping_end as usize - file_load_end as usize;
-            nix::libc::memset(mapping_end as *mut nix::libc::c_void, 0, zero_len);
+            let segment_end = mapped_addr as usize + aligned_size;
+            let mapping_end = align_to_next_page(segment_end);
+            let file_load_end = mapped_addr as usize + page_offset + file_size as usize;
+            let zero_len = mapping_end - file_load_end;
+            println!(
+                "zeroing out from {:x} to {:x}",
+                file_load_end,
+                file_load_end + zero_len
+            );
+            nix::libc::memset(file_load_end as *mut nix::libc::c_void, 0, zero_len);
         }
     }
-    // dbg!(segment.address());
-    if base_addr.is_null() {
-        *base_addr = mapped_addr as *const u8;
+}
+
+fn generate_suitable_base_addr(elf: &ElfFile64) -> *const u8 {
+    let unaligned_end = elf
+        .segments()
+        .map(|segment| segment.address() + segment.size())
+        .max()
+        .unwrap_or(0);
+    let end = align_to_next_page(unaligned_end as usize);
+
+    let base_addr = unsafe {
+        nix::libc::mmap(
+            std::ptr::null_mut(),
+            end,
+            0,
+            nix::libc::MAP_PRIVATE | nix::libc::MAP_ANONYMOUS,
+            -1,
+            0,
+        )
+    };
+
+    unsafe {
+        nix::libc::munmap(base_addr, end);
     }
+
+    base_addr as *const u8
 }
 
 pub fn load_object(name: &str) -> usize {
@@ -121,10 +160,10 @@ pub fn load_object(name: &str) -> usize {
 
     let mut rel_data = object_data.clone();
 
-    let mut base_addr = std::ptr::null();
+    let mut base_addr = generate_suitable_base_addr(&elf);
     for segment in elf.segments() {
         unsafe {
-            load_segment(segment, fd, &mut base_addr);
+            load_segment(segment, fd, base_addr);
         }
     }
 
