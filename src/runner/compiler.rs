5164e04fbb9499fef148d6d9420448ffc2a5275f
StackDoubleFlow <ojaslandge@gmail.com>
2026-06-22 14:53:57 -0500

    Work out integer register allocation

--- src/runner/compiler.rs
+++ src/runner/compiler.rs
@@ -1,5 +1,26 @@
 use crate::runner::CompiledChunk;
 
+// Allocation for all 16 x86_64 registers:
+// x0 -> %rdi (1st argument)
+// x31 (sp) -> %rsp (stack pointer)
+// x1 -> %rsi (2nd argument)
+// x2 -> %rdx (3rd argument)
+// x19 -> %rbx (1st callee-saved)
+// x3 -> %rcx (4th argument)
+// x20 -> %r12 (2nd callee-saved)
+// x21 -> %r13 (3rd callee-saved)
+// x4 -> %r8 (5th argument)
+// x29 (fp) -> %rbp (frame pointer)
+// x22 -> %r14 (4th callee-saved)
+// x5 -> %r9 (6th argument)
+// x23 -> %r10 (5th callee-saved -> temporary)
+// x30 -> %r11 (link register -> temporary)
+// %rax (emulation scratch)
+// %r15 (emulation context)
+
+// Translated Aarch64 registers:
+// x0-x5, x19-x24,
+
 pub fn compile_chunk(chunk_addr: usize) -> CompiledChunk {
     todo!()
 }
