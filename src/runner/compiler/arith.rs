04b3da4a940734d7121b59960e902a432d0d7ff4
StackDoubleFlow <ojaslandge@gmail.com>
2026-06-24 06:16:03 -0500

    Some notes on arith 3op instructions

--- src/runner/compiler/arith.rs
+++ src/runner/compiler/arith.rs
@@ -7,6 +7,17 @@ pub fn compile_instr(
 ) -> Result<bool, iced_x86::IcedError> {
     use bad64::Op;
     match arm_instr.op() {
+        Op::ORR {
+            // Given `ORR dest, src1, src2`
+            //
+            // If src1 and dest are not equal, we need to transfer from src1 to dest first, and then perform the operation on dest with src2.
+            //
+            // This is the absolute worst case:
+            // mov rax, [r15 + src1n * 8] ; Load indirect src1
+            // mov [r15 + destn * 8], rax; Transfer to indirect dest
+            // mov rax, [r15 + src2n * 8] ; Load indirect src2
+            // or [r15 + destn * 8], rax ; Perform operand on dest with src2
+        }
         _ => return Ok(false),
     }
 
