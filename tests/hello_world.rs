ff2281594e3b0afa6e964b3094aed8f693790b22
StackDoubleFlow <ojaslandge@gmail.com>
2026-06-29 10:07:34 -0500

    Fix hello world test

--- tests/hello_world.rs
+++ tests/hello_world.rs
@@ -4,6 +4,7 @@ use assert_cmd::Command;
 fn hello_world() {
     Command::cargo_bin("xob64")
         .unwrap()
+        .arg("./tests/binaries/hello_world")
         .assert()
         .stdout("Hello world!\n")
         .success();
